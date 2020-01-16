2aaef08411baa3474746acb689029bd72affcf35
alexander sonerud <alexandersonerud@hotmail.com>
2020-01-16 11:31:16 +0100

    added IdentityYTProject

---
+++ IdentityYTDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityYTDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
